91f03e9ed6847687dec974b5d2d9f948b755590f
aozp73 <aozp73@naver.com>
2023-02-21 10:09:39 +0900

    UserMapper 인터페이스 생성 완료

---
+++ src/main/java/shop/mtcoding/login_exam/model/UserRepository.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.login_exam.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String eamil);
+
+    public List<User> findByAll();
+
+    public User findById(int id);
+
+    public int updateById(@Param("id") int id, @Param("password") String password,
+            @Param("email") String eamil);
+
+    public int deleteById(int id);
+}
