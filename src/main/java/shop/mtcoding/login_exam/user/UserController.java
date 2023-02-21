5691d6abdfbabc26e589255c148ac2e6636a2636
aozp73 <aozp73@naver.com>
2023-02-21 09:59:41 +0900

    yml, dependencies 설정 및 view 연결 완료

---
+++ src/main/java/shop/mtcoding/login_exam/user/UserController.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.login_exam.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+}
